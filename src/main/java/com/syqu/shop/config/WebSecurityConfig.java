d6e6377736d05e030d2380a5f6bae5c8748bb296
Artaxerxes <artaxerxesnazare@hotmail.com>
2023-05-19 10:51:50 +0100

    permição de acesso ao cart mudada para aceitar apenas user e admin

--- src/main/java/com/syqu/shop/config/WebSecurityConfig.java
+++ src/main/java/com/syqu/shop/config/WebSecurityConfig.java
@@ -45,7 +45,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
-                .antMatchers("/","/home","/index","/about","/help","/register","/cart/**").permitAll()
+                .antMatchers("/","/home","/index","/about","/help","/register").permitAll()
+                .antMatchers("/cart/**").hasAnyRole("USER", "ADMIN")
                 .antMatchers("/user/**").hasRole("USER")
                 .antMatchers("/admin/**","/product/new").hasRole("ADMIN")
                 .and().formLogin().loginPage("/login").permitAll()
